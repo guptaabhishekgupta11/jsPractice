1d337fb8bd1c59d558cafd1d3cd64fd3d0599beb
Abhishek kumar Gupta <114441358+guptaabhishekgupta11@users.noreply.github.com>
2024-04-08 13:55:58 +0000

    just learned new thing on console like console.table and it alsways accepts a object or array

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,8 @@
 console.log("Hello Abhishek");
+
+let city="hyderabad"
+const mobile=7749032471
+var state="Telangana"
+
+//console.table() expects an array or an object as its parameter, not separate variables
+console.table([city,mobile,state])
